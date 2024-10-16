09131bab840b8c9960ec523adc34d1eca9aa8270
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-10-16 17:40:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const makeChange = (c) => {
+  let q = Math.floor(c / 25);
+  c = c % 25;
+  let d = Math.floor(c / 10);
+  c = c % 10;
+  let n = Math.floor(c / 5);
+  let p = c % 5;
+
+  return { q, d, n, p };
   // your name here
 };
 
